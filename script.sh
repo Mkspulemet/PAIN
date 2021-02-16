c41c3755704dd59d7c8c5c05e61e2d936a06750c
mkspulemet <mkspulemet@gmail.com>
2021-02-16 12:57:59 +0300

    we

--- script.sh
+++ script.sh
@@ -9,13 +9,15 @@ sudo apt install php-curl php-json php-cgi php-gd php-zip php-mbstring php-xml p
 
 sudo ufw allow in 80/tcp -y
 
-sudo echo "<virtualHost *:80>" >> /etc/apache2/sites-available/bin.by.conf
-sudo echo "	ServerName bin.by" >> /etc/apache2/sites-available/bin.by.conf
-sudo echo "	ServerAdmin webmaster@host" >> /etc/apache2/sites-available/bin.by.conf
-sudo echo "	DocumentRoot /var/www/bin.by" >> /etc/apache2/sites-available/bin.by.conf
-sudo echo "	ErrorLog \${APACHE_LOG_DIR}/$errlog.log" >> /etc/apache2/sites-available/bin.by.conf
-echo "	CustomLog \${APACHE_LOG_DIR}/$acclog.log combined" >> /etc/apache2/sites-available/bin.by.conf
-echo "</VirtualHost>" > /etc/apache2/sites-available/bin.by.conf
+sudo touch /etc/apache2/sites-availeble/bin.by.conf
+
+sudo echo "<virtualHost *:80>" > /etc/apache2/sites-available/bin.by.conf
+sudo echo "	ServerName bin.by" > /etc/apache2/sites-available/bin.by.conf
+sudo echo "	ServerAdmin webmaster@host" > /etc/apache2/sites-available/bin.by.conf
+sudo echo "	DocumentRoot /var/www/bin.by" > /etc/apache2/sites-available/bin.by.conf
+sudo echo "	ErrorLog \${APACHE_LOG_DIR}/$errlog.log" > /etc/apache2/sites-available/bin.by.conf
+sudo echo "	CustomLog \${APACHE_LOG_DIR}/$acclog.log combined" > /etc/apache2/sites-available/bin.by.conf
+sudo echo "</VirtualHost>" > /etc/apache2/sites-available/bin.by.conf
 
 
 sudo wget https://ru.wordpress.org/latest-ru_RU.zip
@@ -24,10 +26,10 @@ sudo mv /var/www/wordpress/ /var/www/bin.by/
 
 sudo mysql_secure_installation
 
-mysql -e "CREATE DATABASE PULEMET"
-mysql -e "CREATE USER mkspulemet@localhost IDENTIFIED BY '07071989';"
-mysql -e "GRANT ALL PRIVILEGES ON mkspulemet.* TO 'mkspulemet'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE DATABASE PULEMET"
+mysql -u root -e "CREATE USER mkspulemet@localhost IDENTIFIED BY '07071989';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON mkspulemet.* TO 'mkspulemet'@'localhost';"
+mysql -u root -e "FLUSH PRIVILEGES;"
 
 
 sudo wget https://ru.wordpress.org/latest-ru_RU.zip
