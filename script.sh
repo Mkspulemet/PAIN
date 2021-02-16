f99ad865f4e6c2a56a74332aec32abc2f345f64c
mkspulemet <mkspulemet@gmail.com>
2021-02-16 10:59:37 +0300

    script

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-sudo apt install apache2
-sudo apt install mysql-server
-sudo apt install php libapache2-mod-php php-mysql
-sudo apt install php-curl php-json php-cgi php-gd php-zip php-mbstring php-xml php-xmlrpc
+sudo apt install apache2 -y
+sudo apt install mysql-server -y
+sudo apt install php libapache2-mod-php php-mysql -y
+sudo apt install php-curl php-json php-cgi php-gd php-zip php-mbstring php-xml php-xmlrpc -y
 
+sudo ufw allow in 80/tcp -y
 
+sudo mysql_secure_installation
 
