de27b5466c3702f871b533ebd14d5d3542c1323f
Maxim Timofeev <mkspulemet@gmail.com>
2021-02-16 10:37:14 +0300

    script

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt install apache2
+sudo apt install mysql-server
+sudo apt install php libapache2-mod-php php-mysql
+sudo apt install php-curl php-json php-cgi php-gd php-zip php-mbstring php-xml php-xmlrpc
+
+
+
