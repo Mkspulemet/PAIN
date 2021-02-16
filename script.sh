c31aadaef17f94f6809f76bc00fc75049379d3b5
mkspulemet <mkspulemet@gmail.com>
2021-02-16 11:30:57 +0300

    database

--- script.sh
+++ script.sh
@@ -8,3 +8,12 @@ sudo ufw allow in 80/tcp -y
 
 sudo mysql_secure_installation
 
+#create passwd
+PASSWDDB="$(openssl rand -base64 12)"
+#create username
+MAINDB=${USER_NAME//[^a-zA-Z0-9]/_}
+#create database
+mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+mysql -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+mysql -e "FLUSH PRIVILEGES;"
